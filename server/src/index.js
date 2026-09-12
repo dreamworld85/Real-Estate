b9a851741edea9cfdd1a48efeab16e56f323883a
Jayapraveen K <117831424+dreamworld85@users.noreply.github.com>
2026-09-12 23:18:51 +0530

    Fix dotenv initialization order in server/src/index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+dotenv.config({ override: true });
 import path from "path";
 import fs from "fs";
 import { fileURLToPath } from "url";
